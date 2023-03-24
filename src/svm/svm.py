7a33a6515a5ce37dc049fae194c2baaed0f33913
wasphulud <data.driven.wasp@gmail.com>
2023-03-24 16:04:44 +0100

    feat: implement maxmarginclassifier

---
+++ src/svm/svm.py
@@ -0,0 +1,312 @@
+""" This module contains the implementation of the SVM algorithm. """
+
+
+from sklearn.utils import shuffle
+from scipy.optimize import minimize
+import numpy as np
+
+from abc_models.models import SupervisedTabularDataModel
+
+
+class SVC(SupervisedTabularDataModel):
+    """Implementation of the SVC algorithm
+
+    TODO: implement the kernel trick
+    TODO: implement the dual form
+    TODO: Re-implement the optimizer
+    TODO: Cythonization ?"""
+
+    weights: np.ndarray = np.array([])
+    bias: float = 0
+
+    def __init__(
+        self, learning_rate: float, lambda_rate: float, iterations: int = 1000
+    ) -> None:
+        super().__init__()
+        self.learning_rate = learning_rate
+        self.iterations = iterations
+        self.lambda_rate = lambda_rate
+
+    def _set_init_weights(self, data: np.ndarray) -> None:
+        """Set the initial weights and bias"""
+        self.weights = np.zeros(data.shape[1])
+        self.bias = 0
+
+    def _fit(self, data: np.ndarray, target: np.ndarray) -> "SVC":
+        """_summary_
+
+        Args:
+            data (np.ndarray): the training data
+            target (np.ndarray): the training labels
+
+        Returns:
+            SVC: the class model
+        """
+        self._set_init_weights(data)
+        for _ in range(self.iterations):
+            x_data, y_target = shuffle(data, target)  # type: ignore
+            for idx, x_train in enumerate(x_data):
+                y_train: float = y_target[idx]
+                condition = y_train * (np.dot(self.weights, x_train) + self.bias) < 1
+                gradient_w, gradient_b = self._compute_gradient(
+                    condition, x_train, y_train
+                )
+
+                self._update_weights(gradient_w, gradient_b)
+        return self
+
+    def _predict(self, data: np.ndarray) -> np.ndarray:
+        # Predict the target values
+        prediction = np.dot(data, self.weights) + self.bias
+        return np.sign(prediction)
+
+    def _update_weights(self, gradient_w: np.ndarray, gradient_b: float) -> None:
+        """This method updates the weights and bias
+
+        Args:
+            gradient_w (np.ndarray): the gradient of the weights
+            gradient_b (float): the gradient of the bias
+        """
+
+        self.weights -= self.learning_rate * gradient_w
+        self.bias -= self.learning_rate * gradient_b
+
+    def _compute_gradient(
+        self, condition: bool, x_train: np.ndarray, y_train: float
+    ) -> tuple[np.ndarray, float]:
+        """This method computes  the gradient descent
+
+        Args:
+            condition (bool): support vectors condition
+            x_train (np.ndarray): training input
+            y_train (np.ndarray): training input label
+
+        Returns:
+            _type_: _description_
+        """
+
+        if condition:
+            gradient_w = self.lambda_rate * self.weights - x_train * y_train
+            gradient_b = -y_train
+        else:
+            gradient_w = self.lambda_rate * self.weights
+            gradient_b = 0
+        return gradient_w, gradient_b
+
+
+class MaxMarginClassifier(SupervisedTabularDataModel):
+    """Implementation of the Max Margin Classifier algorithm"""
+
+    alpha: np.ndarray = np.array([])
+    weights: np.ndarray = np.array([])
+    support_vectors: np.ndarray = np.array([])
+    intercept: float = 0
+
+    def _fit(self, dataframe: np.ndarray, target: np.ndarray) -> "MaxMarginClassifier":
+        """training the MMC model
+
+        Args:
+            data (np.ndarray): the training data
+            target (np.ndarray): the training labels
+
+        Returns:
+            MaxMarginClassifier: the class model
+        """
+
+        # our goal is to minimize L_d(alpha)=\sum\alpha_i -1/2\sum\sum<\alpha_iy_ix_i,\alpha_ky_kx_k>
+        # thus maximize -L_d(alpha)
+        # we will create the gram matrix of Xy (<yixi,ykxk>)
+        # we will then impelment the L_d(alpha) as a function of alpha
+        # and then use the scipy.optimize.minimize to find the optimal alpha
+        # then we will use the alpha to compute the weights and the bias
+        # and finally we will use the weights and the bias to predict the target values
+
+        # create the gram matrix
+        # Gram_XY = (X.Y) * transpose(X.Y)
+        gram_datay = np.dot(
+            dataframe * target[:, np.newaxis], (dataframe * target[:, np.newaxis]).T
+        )
+
+        # constrains on alpha
+        identity_matrix = np.eye(len(target))
+
+        cons = (
+            {
+                "type": "eq",
+                "fun": lambda a: self.constraint(a, target),
+                "jac": lambda a: target,
+            },
+            {"type": "ineq", "fun": lambda a: a, "jac": lambda a: identity_matrix},
+        )
+
+        # maximize by minimizing the opposite
+        optimization_result = minimize(
+            fun=lambda a: -self.objective(gram_datay, a),
+            x0=np.zeros(len(target)),
+            jac=lambda a: -self.objective_derivative(gram_datay, a),
+            constraints=cons,
+            method="SLSQP",
+        )
+
+        # get the optimal alpha
+        self.alpha = optimization_result.x
+        self.weights = np.dot(self.alpha * target, dataframe)
+        epsilon = 1e-6
+        #  alpha is sparse and strong duality means is alpha > 0 then yi(<weights, xi> +b) = 1
+        # if yi(<weights, xi> +b) > 1 the distance of xi to the hyperplan is larger than the margin.
+        self.support_vectors = dataframe[self.alpha > epsilon]
+        support_labels = target[self.alpha > epsilon]
+
+        # using a support vector x with target y: b = target - <weights, x>
+        self.intercept = support_labels[0] - np.matmul(
+            self.support_vectors[0], self.weights
+        )
+
+        return self
+
+    def _predict(self, dataframe: np.ndarray) -> np.ndarray:
+        """Predict y value in {-1, 1}"""
+        return 2 * (np.matmul(dataframe, self.weights) > 0) - 1
+
+    @staticmethod
+    def objective(gram: np.ndarray, alpha: np.ndarray) -> float:
+        """define the objective function"""
+        return alpha.sum() - 0.5 * alpha.dot(alpha.dot(gram))
+
+    @staticmethod
+    def objective_derivative(gram: np.ndarray, alpha: np.ndarray) -> float:
+        """define the partial derivative of the objective function on alpha"""
+        return np.ones_like(alpha) - np.dot(alpha, gram)
+
+    @staticmethod
+    def constraint(alpha: np.ndarray, target: np.ndarray) -> float:
+        """constraints"""
+        return np.dot(alpha, target)
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as pltcolors
+from sklearn import linear_model, svm, discriminant_analysis, metrics
+from scipy import optimize
+import seaborn as sns
+
+
+def plotLine(ax, xRange, w, x0, label, color="grey", linestyle="-", alpha=1.0):
+    """Plot a (separating) line given the normal vector (weights) and point of intercept
+    """
+    if type(x0) == int or type(x0) == float or type(x0) == np.float64:
+        x0 = [0, -x0 / w[1]]
+    yy = -(w[0] / w[1]) * (xRange - x0[0]) + x0[1]
+    ax.plot(xRange, yy, color=color, label=label, linestyle=linestyle)
+
+
+def plotSvm(
+    X,
+    y,
+    support=None,
+    w=None,
+    intercept=0.0,
+    label="Data",
+    separatorLabel="Separator",
+    ax=None,
+    bound=[[-1.0, 1.0], [-1.0, 1.0]],
+):
+    """Plot the SVM separation, and margin"""
+    if ax is None:
+        fig, ax = plt.subplots(1, projection="3d")
+    im = ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y, cmap=cmap, alpha=0.5, label=label)
+    if support is not None:
+        ax.scatter(
+            support[:, 0],
+            support[:, 1],
+            label="Support",
+            s=80,
+            facecolors="none",
+            edgecolors="y",
+            color="y",
+        )
+        print("Number of support vectors = %d" % (len(support)))
+    if w is not None:
+        xx = np.array(bound[0])
+        plotLine(ax, xx, w, intercept, separatorLabel)
+        # Plot margin
+        if support is not None:
+            signedDist = np.matmul(support, w)
+            margin = np.max(signedDist) - np.min(signedDist) * np.sqrt(np.dot(w, w))
+            supportMaxNeg = support[np.argmin(signedDist)]
+            plotLine(ax, xx, w, supportMaxNeg, "Margin -", linestyle="-.", alpha=0.8)
+            supportMaxPos = support[np.argmax(signedDist)]
+            plotLine(ax, xx, w, supportMaxPos, "Margin +", linestyle="--", alpha=0.8)
+            ax.set_title("Margin = %.3f" % (margin))
+    ax.legend(loc="upper left")
+    ax.grid()
+    ax.set_xlim(bound[0])
+    ax.set_ylim(bound[1])
+    cb = plt.colorbar(im, ax=ax)
+    loc = np.arange(-1, 1, 1)
+    cb.set_ticks(loc)
+    cb.set_ticklabels(["-1", "1"])
+
+
+colors = ["blue", "red"]
+cmap = pltcolors.ListedColormap(colors)
+nFeatures = 3
+N = 100
+
+
+def generateBatchBipolar(n, mu=0.5, sigma=0.2):
+    """Two gaussian clouds on each side of the origin"""
+    X = np.random.normal(mu, sigma, (n, nFeatures))
+    yB = np.random.uniform(0, 1, n) > 0.5
+    # y is in {-1, 1}
+    y = 2.0 * yB - 1
+    X *= y[:, np.newaxis]
+    X -= X.mean(axis=0)
+    return X, y
+
+
+xTrain0, yTrain0 = generateBatchBipolar(N, sigma=0.2)
+model00 = MaxMarginClassifier()
+model00.fit(xTrain0, yTrain0)
+print(model00.weights, model00.intercept)
+print(model00.support_vectors)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
+
+# fig, ax = plt.subplots(1, figsize=(12, 7))
+plotSvm(
+    xTrain0,
+    yTrain0,
+    model00.support_vectors,
+    model00.weights,
+    model00.intercept,
+    label="Training",
+    ax=ax,
+)
+
+# plt.show()
+from time import sleep
+
+# sleep(3*60)
+# save figure
+fig.savefig("svc.png")
+
+model01 = svm.SVC(kernel="linear", gamma="auto", C=1e6)
+model01.fit(xTrain0, yTrain0)
+print(model01.coef_, model01.intercept_)
+print(model01.dual_coef_)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
+plotSvm(
+    xTrain0,
+    yTrain0,
+    model01.support_vectors_,
+    model01.coef_[0],
+    model01.intercept_[0],
+    label="Training",
+    ax=ax,
+)
+fig.savefig("svc_sklearn.png")
